0b97bcd2215c77fde76d4d4eff5bff9a5d71b65f
masterdesky <masterdesky@gmail.com>
2020-12-02 18:12:17 +0100

    adding hw11

--- Homeworks/Week 11/hw_utils.py
+++ Homeworks/Week 11/hw_utils.py
@@ -13,6 +13,7 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 from sklearn.metrics import confusion_matrix
 from sklearn.preprocessing import label_binarize
+from sklearn.metrics import roc_curve, auc, roc_auc_score
 
 axistitlesize = 20
 axisticksize = 17
@@ -49,7 +50,7 @@ def accuracy_metric(y_test, y_pred):
 
 
 def plot_confusion_matrix(conf_mat, y, labels=None, title=None,
-                          textsize=26):
+                          textsize=26, save=False, save_filename='image'):
     """
     Plots a confusion matrix.
     """
@@ -95,5 +96,59 @@ def plot_confusion_matrix(conf_mat, y, labels=None, title=None,
 
     plt.suptitle(title,
                  fontsize=axistitlesize+5, y=0.1)
+    
+    if save:
+        if not os.path.exists(out):
+            os.makedirs(out)
+        f = save_filename.split('.')
+        fn = f[0]
+        ff = 'png' if len(f) == 1 else f[1]
+        plt.savefig(out + fn + '.' + ff,
+                    format=ff, dpi=200,
+                    facecolor='black', edgecolor='black',
+                    bbox_inches='tight')
 
     plt.show()
+    
+def compute_roc(estimator, X, y):
+    """
+    Creates the ROC curve and computes AUC values an input X-y data-target set.
+    
+    Paramters
+    ---------
+    estimator : estimator instance
+        Fitted classifier or a fitted :class:`~sklearn.pipeline.Pipeline`
+        in which the last estimator is a classifier.
+    
+    X : {array-like, sparse matrix} of shape (n_test, n_features)
+        Input values.
+    
+    y : array-like of shape (n_train,)
+        Target values.
+    
+    Returns
+    -------
+    fpr : ndarray
+        False positive rates.
+    tpr : ndarray
+        True positive rates.
+    roc_auc : float
+        Area under ROC curves.
+    """
+    from sklearn.metrics import _plot
+    
+    # Calculate scores for ROC curve
+    # Small hack because I want to automate it. Also I have no trust in S.O. currently.
+    # Using this: https://github.com/scikit-learn/scikit-learn/blob/0fb307bf3/sklearn/metrics/_plot/roc_curve.py#L114
+    c_method = _plot.base._check_classifer_response_method(estimator,
+                                                           response_method='auto')
+    y_score = c_method(X)
+
+    # Calculate ROC and AUC values
+    pos_label = estimator.classes_[1]
+    if y_score.ndim != 1:
+        y_score = y_score[:, 1]
+    fpr, tpr, _ = roc_curve(y, y_score, pos_label=pos_label)
+    roc_auc = auc(fpr, tpr)
+    
+    return fpr, tpr, roc_auc
